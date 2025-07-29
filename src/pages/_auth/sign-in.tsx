d063ffd07a4fa86c415fb77024e2c457d8ba0ef8
carlospinellowork <carlos.pinello@outlook.com>
2025-07-29 09:11:12 -0300

    feat: add origin ui

--- src/pages/_auth/sign-in.tsx
+++ src/pages/_auth/sign-in.tsx
@@ -4,6 +4,8 @@ import { Label } from '@/components/ui/label';
 import { useAuth } from '@/context/authContext';
 import { useMutation } from '@tanstack/react-query';
 import { createFileRoute, redirect, useNavigate } from '@tanstack/react-router';
+import { EyeIcon, EyeOffIcon, MailIcon } from 'lucide-react';
+import { useState } from 'react';
 
 export const Route = createFileRoute('/_auth/sign-in')({
   beforeLoad: async ({ context }) => {
@@ -19,6 +21,9 @@ export const Route = createFileRoute('/_auth/sign-in')({
 function SignIn() {
   const { login } = useAuth();
   const navigate = useNavigate();
+  const [isVisible, setIsVisible] = useState<boolean>(false);
+
+  const toggleVisibility = () => setIsVisible((prevState) => !prevState);
 
   const mutation = useMutation({
     mutationFn: async ({
@@ -57,35 +62,57 @@ function SignIn() {
       </div>
 
       <div className="grid gap-2">
-        <Label htmlFor="email">E-mail</Label>
-        <Input
-          id="email"
-          name="email"
-          type="email"
-          placeholder="m@example.com"
-          required
-          disabled={mutation.isPending}
-        />
+        <div className="*:not-first:mt-2">
+          <Label>Email</Label>
+          <div className="relative">
+            <Input
+              className="peer pe-9"
+              placeholder="Email"
+              type="email"
+              disabled={mutation.isPending}
+            />
+            <div className="text-muted-foreground/80 pointer-events-none absolute inset-y-0 end-0 flex items-center justify-center pe-3 peer-disabled:opacity-50">
+              <MailIcon size={16} aria-hidden="true" />
+            </div>
+          </div>
+        </div>
       </div>
 
       <div className="grid gap-2">
-        <div className="flex items-center">
-          <Label htmlFor="password">Senha</Label>
-          <a
-            href="#"
-            className="ml-auto text-sm underline-offset-2 hover:underline"
-          >
-            Esqueceu sua senha?
-          </a>
+        <div className="*:not-first:mt-2">
+          <Label>Senha</Label>
+
+          <div className="relative">
+            <Input
+              className="pe-9"
+              placeholder="Senha"
+              type={isVisible ? 'text' : 'password'}
+              disabled={mutation.isPending}
+            />
+            <button
+              className="text-muted-foreground/80 hover:text-foreground focus-visible:border-ring focus-visible:ring-ring/50 absolute inset-y-0 end-0 flex h-full w-9 items-center justify-center rounded-e-md transition-[color,box-shadow] outline-none focus:z-10 focus-visible:ring-[3px] disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50"
+              type="button"
+              onClick={toggleVisibility}
+              aria-label={isVisible ? 'Hide password' : 'Show password'}
+              aria-pressed={isVisible}
+              aria-controls="password"
+            >
+              {isVisible ? (
+                <EyeOffIcon size={16} aria-hidden="true" />
+              ) : (
+                <EyeIcon size={16} aria-hidden="true" />
+              )}
+            </button>
+          </div>
         </div>
-        <Input
-          id="password"
-          name="password"
-          type="password"
-          required
-          disabled={mutation.isPending}
-        />
       </div>
+
+      <a
+        href="#"
+        className="ml-auto text-sm underline-offset-2 hover:underline"
+      >
+        Esqueceu sua senha?
+      </a>
 
       <Button type="submit" className="w-full" disabled={mutation.isPending}>
         {mutation.isPending ? 'Entrando...' : 'Entrar'}
